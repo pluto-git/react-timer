bcbfa1200e1151688635dfb0742414569e6c8e54
DisTans <m.lubachenko@gmail.com>
2021-04-24 15:55:00 +0300

    1.0

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import classes from "./StopWatch.module.css";
+import { timer, interval } from "rxjs";
 
 const StopWatch = () => {
   const [isActive, setIsActive] = useState(false);
   const [time, setTime] = useState(0);
   const countRef = useRef(null);
+
   //click handling state
   const [clickCounter, setClickCounter] = useState(0);
 
@@ -16,29 +18,29 @@ const StopWatch = () => {
   //handling Start
   const startHandler = () => {
     setIsActive((prevBool) => !prevBool);
-    countRef.current = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
+    countRef.current = timer(1000, 1000).subscribe(() =>
+      setTime((prevTime) => prevTime + 1)
+    );
   };
 
   //handling Stop
   const stopHandler = () => {
-    clearInterval(countRef.current);
+    countRef.current.unsubscribe();
     setTime(0);
     setIsActive((prevBool) => !prevBool);
   };
 
   //handling Wait
   const waitHandler = () => {
-    clearInterval(countRef.current);
-    setIsActive((prevBool) => !prevBool);
+    countRef.current.unsubscribe();
+    setIsActive(false);
   };
 
   //handling Reset
   const resetHandler = () => {
-    clearInterval(countRef.current);
+    countRef.current.unsubscribe();
     setTime(0);
-    setIsActive((prevBool) => !prevBool);
+    setIsActive(false);
 
     startHandler();
   };
@@ -46,8 +48,7 @@ const StopWatch = () => {
   //handling Double Click
   const clickCounterHandler = () => {
     setClickCounter((prevNumber) => prevNumber + 1);
-
-    //as state is not updated immediately for the next line
+    //as state is not updated immediately for the next line we can use "1" instead of "2".
     if (clickCounter === 1) {
       waitHandler();
     }
@@ -71,7 +72,7 @@ const StopWatch = () => {
   return (
     <>
       <header className={classes.header}>
-        <h1>StopWatch via React.js!</h1>
+        <h1>StopWatch!</h1>
       </header>
       <h2>{timeFormatting()} </h2>
 
